3be6bff7ce1fcfd7559accbee631c3206d333991
Till Hoffmann <tillahoffmann@gmail.com>
2020-02-27 09:46:11 +0000

    Add package (fixes #12).

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='asymmetric_kde',
-      version='1.0',
-      description='Asymmetric kernel density estimation',
-      author='Till Hoffmann',
-      author_email='tah13@imperial.ac.uk',
-      url='https://github.com/tillahoffmann/asymmetric_kde',
-      requires=['numpy', 'scipy']
-      )
+setup(
+    name='asymmetric_kde',
+    version='1.1',
+    description='Asymmetric kernel density estimation',
+    author='Till Hoffmann',
+    author_email='tah13@imperial.ac.uk',
+    url='https://github.com/tillahoffmann/asymmetric_kde',
+    requires=['numpy', 'scipy'],
+    packages=find_packages(),
+)
